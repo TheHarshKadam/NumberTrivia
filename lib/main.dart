1b51e49efcdccc9931bac0554e2830b9c4599e1b
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-05-28 23:01:58 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:number_trivia/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: splash_screen(),
+  ));
+}
+
+class splash_screen extends StatefulWidget {
+  @override
+  _splash_screenState createState() => _splash_screenState();
+}
+
+class _splash_screenState extends State<splash_screen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+          ()=>Navigator.pushReplacement(context,
+              MaterialPageRoute(builder:
+                (context) =>home()
+              )
+          )
+     );
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: FlutterLogo(size: MediaQuery.of(context).size.height,),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('THIS IS HOME PAGE'),
+    );
+  }
+}
