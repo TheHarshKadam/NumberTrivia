00a591a2f3a1af3fcffcee319d9e4beee14fb680
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-06-01 23:37:41 +0530

    Added Dialog Box

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,50 +9,85 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
+    void _showDialog() {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Enter your lucky number'),
+              content: Text('ALert dialog body'),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"),
+                )
+              ],
+            );
+          });
+    }
+
     return Scaffold(
       backgroundColor: Colors.black54,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 50.0,),
+          SizedBox(
+            height: 80.0,
+          ),
           Text(
             'Number Trivia',
             style: GoogleFonts.carroisGothic(
-             fontSize:40.0,
-             fontWeight: FontWeight.bold,
-             color: Colors.pinkAccent[400]
-            ),
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.pinkAccent[400]),
           ),
-          SizedBox(height: 30.0,),
+          SizedBox(
+            height: 30.0,
+          ),
           Padding(
-            padding: const EdgeInsets.only(top: 400.0),
+            padding: const EdgeInsets.only(top: 350.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {},
+                  onLongPress: () {
+                    return _showDialog();
+                  },
                   child: Text('Random Year'),
                 ),
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {
+                    print('DATE');
+                  },
+                  onLongPress: () {
+                    print('Dialog Box 1');
+                  },
                   child: Text('Random Dates'),
                 ),
               ],
             ),
           ),
-          SizedBox(height: 20.0,),
+          SizedBox(
+            height: 20.0,
+          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Padding(
                 padding: const EdgeInsets.fromLTRB(20.0, 0.0, 10.0, 20.0),
                 child: ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {
+                    print('TRIVIA');
+                  },
+                  onLongPress: () {
+                    print("Dialog box 3");
+                  },
                   child: Text(
                     'RANDOM TRIVIA',
-                    style: TextStyle(
-                      fontSize: 20.0
-                    ),
+                    style: TextStyle(fontSize: 20.0),
                   ),
                 ),
               ),
