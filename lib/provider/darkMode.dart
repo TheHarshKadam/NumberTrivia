86969d425cdefd95a6917ba88ca8d9bba1507e16
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-06-11 01:29:11 +0530

    Added Dark Mode

---
+++ lib/provider/darkMode.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.dark;
+  bool get isDarkMode {
+    return themeMode == ThemeMode.dark;
+  }
+
+  void toggleTheme(bool isOn) {
+    themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
+class MyTheme {
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.black54,
+    colorScheme: ColorScheme.dark(),
+  );
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    colorScheme: ColorScheme.light(),
+  );
+}
