95df4e2ff4d3f90341bc970354aea9c4a9b979d5
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-06-11 01:33:11 +0530

    Added System & User Dark Mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
-//Added Splash Screen
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:number_trivia/pages/home.dart';
+import 'package:number_trivia/provider/darkMode.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: splash_screen(),
+  runApp(ChangeNotifierProvider(
+    create: (context) {
+      return ThemeProvider();
+    },
+    // ignore: missing_return
+    builder: (context,_) {
+      final themeProvider = Provider.of<ThemeProvider>(context);
+        return MaterialApp(
+        themeMode: themeProvider.themeMode,
+        theme: MyTheme.lightTheme,
+        darkTheme: MyTheme.darkTheme,
+        debugShowCheckedModeBanner: false,
+        home: splash_screen(),
+      );
+    },
   ));
 }
 
@@ -20,19 +32,23 @@ class _splash_screenState extends State<splash_screen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-          ()=>Navigator.pushReplacement(context,
-              MaterialPageRoute(builder:
-                (context) =>home()
-              )
-          )
-     );
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => home())));
   }
 
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: FlutterLogo(size: MediaQuery.of(context).size.height,),
-    );
+    return Scaffold(
+        //backgroundColor: Colors.white,
+        body: Center(
+      child: Image(
+        image: AssetImage(
+          'assets/facts.png',
+        ),
+        width: 300.0,
+        height: 300.0,
+      ),
+    ));
   }
 }
